5a432e785c52790050137ef083dfb3bbb146340c
Jude Boachie <jude.boachie@amalitech.com>
2026-01-26 17:57:38 +0000

    Extract editor into standalone component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Editor } from './components/editor/editor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Editor],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
